23b5489725aa9ea0fb283f50447c397599d091af
TanimaRanjan <tanimaranjan@gmail.com>
2020-06-04 11:06:01 -0400

    Implemented show and delete functionality on portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -42,6 +42,16 @@ class PortfoliosController < ApplicationController
     end
 
 
+    def destroy
+
+        @portfolio_item = Portfolio.find(params[:id])
+
+        @portfolio_item.destroy
+        respond_to do |format|
+            format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully deleted '}
+        end
+    end
+
     def portfolio_item_params
         params.require(:portfolio).permit(:title, :subtitle, :body)
     end 
