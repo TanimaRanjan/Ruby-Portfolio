8fe09759495f2def41eb71ec3e5100fab1e42fca
TanimaRanjan <tanimaranjan@gmail.com>
2020-06-03 21:48:09 -0400

    Created seed file for the project

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+    Blog.create!(
+        title:"My Blog Post #{blog}",
+        body:"Lemon drops cheesecake danish cupcake halvah dragée tart. Lemon drops macaroon icing pastry chocolate bar chocolate bar sweet. Gingerbread cupcake wafer. Macaroon oat cake candy cupcake cookie. Chupa chups carrot cake chocolate cake cotton candy. Biscuit chupa chups powder brownie icing topping dessert chocolate cake. Bonbon caramels jelly-o chupa chups chocolate bar marzipan. Wafer liquorice gummies sesame snaps oat cake sesame snaps. Gingerbread candy canes candy canes pie. Gummies fruitcake halvah lemon drops. Soufflé oat cake gummi bears brownie icing. Cake chocolate bar cupcake dragée candy candy canes dragée caramels. Marzipan jelly dragée carrot cake sesame snaps brownie lemon drops powder. Ice cream pastry chocolate bar gummies lollipop cake cake."
+    )
+end
+
+puts "10 Blog post created"
+
+5.times do |skill|
+    Skill.create!(
+        title:"Rails #{skill}",
+        percent_utilized: 20
+    )
+end
+
+puts "5 Skills created"
+
+9.times do |portfolio_item|
+    Portfolio.create!(
+        title:"Portfolio Title #{portfolio_item}",
+        subtitle:"Client Info #{portfolio_item} ",
+        body:"Gingerbread soufflé donut. Sesame snaps liquorice icing biscuit muffin caramels chocolate. Cupcake croissant sweet lollipop jelly-o candy canes powder. Jelly macaroon jelly beans sesame snaps cheesecake pie topping sweet. Sesame snaps muffin brownie topping topping. Pie topping sweet lemon drops lollipop. ",
+        main_image:"https://picsum.photos/600/600?random=#{portfolio_item}",
+        thumb_image:"https://picsum.photos/200/200?random=#{portfolio_item}"
+    )
+end
+
+
+puts "9 portfolio items created"
