33e483462e70327be9d9ff580248d34dae3b0880
TanimaRanjan <tanimaranjan@gmail.com>
2020-06-04 16:13:55 -0400

    Toggle status between draft and published

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,13 @@ Rails.application.routes.draw do
   # get 'pages/about'
   # get 'pages/contact'
   
-  resources :blogs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+  
+  
   root to: 'pages#home'
 
 end
