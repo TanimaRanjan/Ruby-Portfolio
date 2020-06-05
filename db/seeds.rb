9a3116d04c8d332f904e065849583c75e161adcd
TanimaRanjan <tanimaranjan@gmail.com>
2020-06-05 08:08:22 -0400

    Created custom scope

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,67 @@
+3.times do |topic|
+    Topic.create!(
+        title: "Topic #{topic}"
+    )
+end
+
+
 10.times do |blog|
     Blog.create!(
         title:"My Blog Post #{blog}",
-        body:"Lemon drops cheesecake danish cupcake halvah dragée tart. Lemon drops macaroon icing pastry chocolate bar chocolate bar sweet. Gingerbread cupcake wafer. Macaroon oat cake candy cupcake cookie. Chupa chups carrot cake chocolate cake cotton candy. Biscuit chupa chups powder brownie icing topping dessert chocolate cake. Bonbon caramels jelly-o chupa chups chocolate bar marzipan. Wafer liquorice gummies sesame snaps oat cake sesame snaps. Gingerbread candy canes candy canes pie. Gummies fruitcake halvah lemon drops. Soufflé oat cake gummi bears brownie icing. Cake chocolate bar cupcake dragée candy candy canes dragée caramels. Marzipan jelly dragée carrot cake sesame snaps brownie lemon drops powder. Ice cream pastry chocolate bar gummies lollipop cake cake."
+        body:"Lemon drops cheesecake danish cupcake halvah dragée tart. Lemon drops macaroon icing pastry chocolate bar chocolate bar sweet. Gingerbread cupcake wafer. Macaroon oat cake candy cupcake cookie. Chupa chups carrot cake chocolate cake cotton candy.",
+        topic_id: Topic.last.id
     )
 end
 
 puts "10 Blog post created"
 
-5.times do |skill|
+1.times do |skill|
     Skill.create!(
-        title:"Rails #{skill}",
+        title:"Rails",
+        percent_utilized: 20
+    )
+end
+1.times do |skill|
+    Skill.create!(
+        title:"HTML",
+        percent_utilized: 20
+    )
+end
+1.times do |skill|
+    Skill.create!(
+        title:"React",
+        percent_utilized: 20
+    )
+end
+1.times do |skill|
+    Skill.create!(
+        title:"CSS3",
         percent_utilized: 20
     )
 end
 
 puts "5 Skills created"
 
-9.times do |portfolio_item|
+5.times do |portfolio_item|
     Portfolio.create!(
         title:"Portfolio Title #{portfolio_item}",
-        subtitle:"Client Info #{portfolio_item} ",
-        body:"Gingerbread soufflé donut. Sesame snaps liquorice icing biscuit muffin caramels chocolate. Cupcake croissant sweet lollipop jelly-o candy canes powder. Jelly macaroon jelly beans sesame snaps cheesecake pie topping sweet. Sesame snaps muffin brownie topping topping. Pie topping sweet lemon drops lollipop. ",
+        subtitle:"Ruby on Rails",
+        body:"Gingerbread soufflé donut. Sesame snaps liquorice icing biscuit muffin caramels chocolate. Cupcake croissant sweet lollipop jelly-o candy canes powder. Jelly macaroon jelly beans sesame snaps cheesecake pie topping sweet. Sesame snaps muffin brownie topping topping. ",
         main_image:"https://picsum.photos/600/600?random=#{portfolio_item}",
         thumb_image:"https://picsum.photos/200/200?random=#{portfolio_item}"
+    
     )
 end
 
+4.times do |portfolio_item|
+    Portfolio.create!(
+        title:"Portfolio Title #{portfolio_item}",
+        subtitle:"React",
+        body:"Gingerbread soufflé donut. Sesame snaps liquorice icing biscuit muffin caramels chocolate. Cupcake croissant sweet lollipop jelly-o candy canes powder. Jelly macaroon jelly beans sesame snaps cheesecake pie topping sweet. Sesame snaps muffin brownie topping topping.",
+        main_image:"https://picsum.photos/600/600?random=#{portfolio_item}",
+        thumb_image:"https://picsum.photos/200/200?random=#{portfolio_item}"
+    
+    )
+end
 
 puts "9 portfolio items created"
