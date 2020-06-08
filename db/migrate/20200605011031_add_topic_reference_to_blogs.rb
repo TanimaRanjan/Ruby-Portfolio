6caa96579f2e64f468821cb78d7b535df471022d
TanimaRanjan <tanimaranjan@gmail.com>
2020-06-08 09:55:32 -0400

    Fixing the bug with add new blog item

--- db/migrate/20200605011031_add_topic_reference_to_blogs.rb
+++ db/migrate/20200605011031_add_topic_reference_to_blogs.rb
@@ -1,5 +1,6 @@
 class AddTopicReferenceToBlogs < ActiveRecord::Migration[6.0]
   def change
     add_reference :blogs, :topic,  foreign_key: true
+    # add_column :blogs, :topic_id, :integer
   end
 end
