5e79c46b127dccc2add6f067e7b4119dc3387bff
TanimaRanjan <tanimaranjan@gmail.com>
2020-06-10 09:06:41 -0400

    Copyright gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,18 @@ class ApplicationController < ActionController::Base
 
     include DefaultPageContentConcern
 
+    before_action :set_copyright
+
+    def set_copyright
+        @copyright = TanimaRanjanViewTool::Renderer.copyright 'Tanima Ranjan', 'All rights reserved'
+    end
+
 end
 
+module TanimaRanjanViewTool
+    class Renderer
+        def self.copyright name, message
+            "&copy; #{Time.now.year} | <b> #{name}</b> #{message}".html_safe
+        end
+    end
+end
